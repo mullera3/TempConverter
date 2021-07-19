43cc3420719cdf41081fe2b80791f50fdab7f798
mullera3 <amani.k.muller@gmail.comClotilda1_>
2021-07-18 21:55:32 -0400

    basic temp converter app

--- app/src/main/java/com/example/smartconverter/MainActivity.kt
+++ app/src/main/java/com/example/smartconverter/MainActivity.kt
@@ -9,35 +9,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fField : TextView = findViewById(R.id.fahrenheit_input)
-        var numberToBeConverted  : CharSequence
-        val cField : TextView = findViewById(R.id.celsius_input)
-        val convertButton : Button = findViewById(R.id.convert_button)
-        val resetButton : Button = findViewById(R.id.reset_button)
+        val fField : TextView = findViewById(R.id.fahrenheit_input)  // variable controlling view for fahrenheit field
+        var numberToBeConverted  : CharSequence // holds char sequence data coming from view
+        val cField : TextView = findViewById(R.id.celsius_input) // variable controlling view  for Celsius field
+        val convertButton : Button = findViewById(R.id.convert_button)  // variable controlling convert button view
+        val resetButton : Button = findViewById(R.id.reset_button) // variable controlling reset button view
+
+        // makes app listen for button click
+        // basically when I click it will do what this block says
+        // This listens for convert button click
         convertButton.setOnClickListener{
+            // Checks to see if fields are empty from view
             if (fField.text.toString().isEmpty() && cField.text.toString().isEmpty())
             {
+                // Prints quick message to screen if both fields are empty
                 Toast.makeText(this, "You have to give a number to either input!", Toast.LENGTH_SHORT).show()
             }
+            // Checks to see if one field is not empty
             else if (fField.text.toString().isEmpty() && cField.text.toString().isNotEmpty())
             {
-                numberToBeConverted  = cField.text
+                numberToBeConverted  = cField.text //grabs data from field
                 var convertedNumber : Float = numberToBeConverted.toString().toFloat()
-                convertedNumber = ((convertedNumber * 1.8) + 32).toFloat();
-                fField.text = convertedNumber.toString()
+                // converts data to string then to float
+                convertedNumber = ((convertedNumber * 1.8) + 32).toFloat(); // converts Celsius to fahrenheit
+                fField.text = convertedNumber.toString() // sets view equal to new data
 
             }
+            // Checks to see if one field is not empty
             else
             {
-                numberToBeConverted  = fField.text
+                numberToBeConverted  = fField.text //grabs data from field
+                // converts data to string then to float
                 var convertedNumber : Float = numberToBeConverted.toString().toFloat()
-                convertedNumber = ((convertedNumber - 32) * 5/9);
-                cField.text = convertedNumber.toString()
+                convertedNumber = ((convertedNumber - 32) * 5/9);  // converts fahrenheit to Celsius
+                cField.text = convertedNumber.toString() // sets view equal to new data
 
             }
 
         }
 
+        // makes app listen for button click
+        // basically when I click it will do what this block says
+        // This listens for reset button click
         resetButton.setOnClickListener {
             if (fField.text.toString().isEmpty() && cField.text.toString().isEmpty())
             {
